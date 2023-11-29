82b0599bc4bbfc85164db99ad691fefdbc2cd9f2
tylav03 <tylerlavaway@gmail.com>
2023-11-29 11:10:04 -0500

    Button click POST Request test
    Edited javascript to send a POST request to localhost:8080 and include 1 parameter. Also edited HTML to call submit() func on click

--- post.js
+++ post.js
@@ -1,11 +1,11 @@
-fetch("https://jsonplaceholder.typicode.com/todos", {
-  method: "POST",
-  body: JSON.stringify({
-    userId: 1,
-    title: "Fix my bugs",
-    completed: false
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-});
+function submit(){
+    fetch("localhost:8080", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: 1
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+}
